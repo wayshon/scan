228e7e4aa722ad7980364351d0dbd0dc30d4dcd6
wayshon <wayshon@qq.com>
2019-04-02 22:29:22 +0800

    Initial Commit

---
+++ scan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scan
+//
+//  Created by 王旭 on 2019/4/2.
+//  Copyright © 2019 王旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scan
+//
+//  Created by 王旭 on 2019/4/2.
+//  Copyright © 2019 王旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
